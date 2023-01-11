941734d6e604b3781e2ffc800eed3b6f450bf96f
luciano soares <lucianosoaresdev748@gmail.com>
2023-01-11 11:36:16 -0300

    First project settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
+      <Navbar />
       <h2>Movies Lib</h2>
+      <Outlet />
     </div>
   );
 }
